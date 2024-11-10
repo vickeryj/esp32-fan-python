7c3a09691089cd7df2b679cfa2dd5bf3e5f835d1
Josh Vickery <josh@vickeryj.com>
2024-11-10 17:06:47 -0500

    switch to the pins on the mounted board

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from machine import Pin, reset, PWM
 
 import home_assistant_mqtt
 
-pwm_pin = Pin(12)
+pwm_pin = Pin(17)
 pwm = PWM(pwm_pin)
 pwm.freq(25000)
 
-relay_pin = Pin(13, Pin.OUT)
+relay_pin = Pin(18, Pin.OUT)
 
 home_ass_mqtt = home_assistant_mqtt.HomeAssistantMQTT(pwm, relay_pin)
 
